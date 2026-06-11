67608ad4a4052f34b7001b88c9564371fc05912d
Basic <82835398+Basicdx@users.noreply.github.com>
2026-06-11 15:00:21 +0200

    Update RenewCertifs.sh

--- RenewCertifs.sh
+++ RenewCertifs.sh
@@ -3,11 +3,11 @@
 # a ajouter a cron
 
 # Régénère tous les certificats
-tailscale cert jellyfin.imac-home-server.tail1234.ts.net
-tailscale cert photos.imac-home-server.tail1234.ts.net
-tailscale cert vault.imac-home-server.tail1234.ts.net
-tailscale cert home.imac-home-server.tail1234.ts.net
-tailscale cert dockge.imac-home-server.tail1234.ts.net
+tailscale cert jellyfin.poppi.akita-diatonic.ts.net
+tailscale cert photos.poppi.akita-diatonic.ts.net
+tailscale cert vault.poppi.akita-diatonic.ts.net
+tailscale cert home.poppi.akita-diatonic.ts.net
+tailscale cert dockge.poppi.akita-diatonic.ts.net
 
 # Redémarre Caddy
 docker restart caddy
